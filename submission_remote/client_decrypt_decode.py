1347ea94fd9797701141579b05825868d5735c68
pavel <pavel@lattica.ai>
2026-01-08 00:35:46 +0200

    remove loop over batch samples

--- submission_remote/client_decrypt_decode.py
+++ submission_remote/client_decrypt_decode.py
@@ -9,13 +9,12 @@ local_file_paths = submission_utils.init(int(sys.argv[1]))
 context = pickle.load(open(local_file_paths.PATH_CONTEXT, "rb"))
 sk = pickle.load(open(local_file_paths.PATH_SK, "rb"))
 
-results = []
-for i in range(local_file_paths.BATCH_SIZE):
-    ct_res = pickle.load(open(local_file_paths.get_ct_download_path(i), "rb"))
-    res = toolkit_interface.dec(context, sk, ct_res, False)
-    res = worker_api.load_proto_tensor(res)
-    res = res.argmax()
-    results.append(res)
+ct_res = pickle.load(open(local_file_paths.get_ct_download_path("batch"), "rb"))
+res = toolkit_interface.dec(context, sk, ct_res, False)
+res = worker_api.load_proto_tensor(res)
+if res.ndim == 1:
+    res = res.unsqueeze(0)
+results = res.argmax(axis=1).tolist()
 
 with open(local_file_paths.PREDICTIONS_PATH, "w") as f:
     f.write("\n".join(str(int(r)) for r in results) + "\n")
